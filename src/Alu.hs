18a48f6ae003ea8bb1ebd303523c65dc9ade2b18
pnmdln <paul-nicolas.madelaine@ens.fr>
2017-11-29 22:47:31 +0100

    fancy netlist builder

--- src/Alu.hs
+++ src/Alu.hs
@@ -1,105 +1,65 @@
 module Alu where
 
 import qualified Data.List as List
+import Control.Monad (mapM)
 
 import Netlist.Ast
 import Netlist.Build
 
-data ALU_options = ALU_options { enable_add   :: Argument
-                               , carry_in     :: Argument
-                               , invert_x     :: Argument
-                               , invert_y     :: Argument
-                               , shift_value  :: Argument
-                               , shift_l      :: Argument
-                               , enable_xor   :: Argument
-                               , enable_and   :: Argument
+data ALU_options = ALU_options { opt_enable_carry :: Argument
+                               , opt_carry_in     :: Argument
+                               , opt_enable_xor   :: Argument
+                               , opt_enable_and   :: Argument
+                               , opt_invert_y     :: Argument
+                               , opt_shift_left   :: Argument
+                               , opt_shift_value  :: [Argument]
                                }
 
-data ALU_flags = ALU_flags { carry_out :: Argument
+data ALU_flags = ALU_flags { flag_carry_out :: Argument
                           -- zero
                           -- overflow
                            }
 
-fulladder :: Argument -> Argument -> Argument -> Jazz (Argument, Argument)
-fulladder x y z = do
-  let a = arg x
-  let b = arg y
-  let c = arg z
+fulladder :: (Bit a, Bit b, Bit c) => a -> b -> c -> Jazz (Argument, Argument)
+fulladder a b c = do
   r <- (a /\ b) \/ ((a \/ b) /\ c)
   s <- a <> b <> c
-  return (r, s)
+  return (r,s)
 
-shift_left :: [Argument] -> [Argument] -> Jazz [Argument]
-shift_left zs xs =
-  let aux :: [Argument] -> [Argument] -> Integer -> Jazz [Argument]
-      aux [] xs _ = return xs
-      aux (z:zs) xs i = do let l = List.genericReplicate i (ArgCst [False])
-                           as <- aux zs xs (2*i)
-                           let bs = l ++ List.genericTake (List.genericLength xs - i) as
-                           w <- mux (arg z) (funnel bs) (funnel as)
-                           smash w (List.genericLength as)
+nadder :: (Bit a, Bit b, Bit c) => a -> [b] -> [c] -> Jazz (Argument, [Argument])
+nadder c [] [] = do
+  c <- funk c
+  return (c, [])
+nadder c (x:xs) (y:ys) = do
+  (r, z) <- fulladder c x y
+  (c, zs) <- nadder r xs ys
+  return (c, z:zs)
+
+shiftl :: (Bit a, Bit b) => [a] -> [b] -> Jazz [Argument]
+shiftl xs ys =
+  let n = List.genericLength xs in
+  let aux :: (Bit a, Bit b) => Integer -> [a] -> [b] -> Jazz [Argument]
+      aux i xs [] =
+        mapM funk xs
+      aux i xs (y:ys) = do
+        a <- conc (List.genericReplicate i False) (List.genericTake (n-i) xs)
+        b <- mux y a xs
+        aux (2*i) b ys
   in
-  aux zs xs 1
+  aux 1 xs ys
 
-shift_right zs xs = do ys <- shift_left zs (List.reverse xs)
-                       return $ List.reverse ys
+shiftr :: (Bit a, Bit b) => [a] -> [b] -> Jazz [Argument]
+shiftr xs ys = do
+  zs <- shiftl (List.reverse xs) ys
+  return $ List.reverse zs
 
-nadder :: Argument -> [Argument] -> [Argument] -> Jazz (Argument, [Argument])
-nadder c_in [] [] = return (c_in, [])
-nadder c_in (x:xs) (y:ys) = do
-  (r, z) <- fulladder c_in x y
-  (c_out, zs) <- nadder r xs ys
-  return (c_out, z:zs)
-
-aux :: [Jazz Argument] -> Jazz [Argument]
-aux [] = return []
-aux (x:xs) = do a <- x
-                l <- aux xs
-                return $ a:l
-
-alu :: ALU_options -> Argument -> Argument -> Jazz (ALU_flags, Argument)
-alu options x y = do
-  xs <- smash x 8
-  ys <- smash y 8
-  let w1 = List.map ((<>) (arg $ invert_x options)) (List.map arg xs)
-  let w2 = List.map ((<>) (arg $ invert_y options)) (List.map arg ys)
-  let w3 = List.map (\(x,y) -> (x <> y) /\ (arg $ enable_xor options)) (zip w1 w2)
-  let w4 = List.map (\(x,y) -> x /\ y /\ (arg $ enable_and options)) (zip w1 w2)
-  let w5 = List.map (\(x,y) -> x \/ y) (zip w3 w4)
-  l1 <- aux w5
-  (c_out, l2) <- nadder (carry_in options) xs ys
-  w1 <- mux (arg $ enable_add options) (funnel l2) (funnel l1)
-  l1 <- smash w1 8
-  shft <- smash (shift_value options) 4
-  l2 <- shift_left shft l1
-  l3 <- shift_right shft l2
-  w1 <- mux (arg $ shift_l options) (funnel l2) (funnel l3)
-  let flags = ALU_flags { carry_out = c_out
-                        }
-  return (flags, w1)
-
-foo = do
-  x <- input "x" 8
-  y <- input "y" 8
-  o1 <- input "enable_add" 1
-  o2 <- input "carry_in" 1
-  o3 <- input "invert_x" 1
-  o4 <- input "invert_y" 1
-  o5 <- input "shift_value" 4
-  o6 <- input "shift_left" 1
-  o7 <- input "enable_xor" 1
-  o8 <- input "enable_and" 1
-  let options = ALU_options { enable_add  = o1
-                            , carry_in    = o2
-                            , invert_x    = o3
-                            , invert_y    = o4
-                            , shift_value = o5
-                            , shift_l     = o6
-                            , enable_xor  = o7
-                            , enable_and  = o8
-                            }
-  (r,z) <- alu options x y
-  output "z" 8 (arg z)
+foo :: Jazz ()
+foo = do x <- input "x" 8
+         y <- input "y" 3
+         v <- shiftl x y
+         w <- shiftr x y
+         output "v" v
+         output "w" w
 
 netlist = build foo
 
--- src/Netlist/Build.hs
+++ src/Netlist/Build.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+
 module Netlist.Build where
 
 import qualified Data.Map.Strict as Map
@@ -5,9 +8,14 @@ import qualified Data.List as List
 import qualified Data.Set as Set
 import Data.Map.Lazy ((!))
 
+import Data.Functor.Identity (Identity)
+
 import Netlist.Ast
 import Netlist.Show
 import Control.Monad.State.Lazy
+
+addr_size = 8
+word_size = 8
 
 data Env = Env { env_ids   :: Map.Map Expression Ident
                , env_in    :: Set.Set Ident
@@ -16,40 +24,36 @@ data Env = Env { env_ids   :: Map.Map Expression Ident
                }
 
 type Jazz = State Env
-type Funk = Jazz ()
+
+class Bit a where
+  funk :: a -> Jazz Argument
+
+instance Bit Argument where
+  funk x = return x
+
+instance Bit (Jazz Argument) where
+  funk x = x
+
+instance Bit Bool where
+  funk x = return $ ArgCst [x]
+
+instance Bit Integer where
+  funk 0 = return $ ArgCst [False]
+  funk 1 = return $ ArgCst [True]
 
 add_exp :: Expression -> Integer -> Jazz Ident
-add_exp exp n = do s <- get
-                   case Map.lookup exp (env_ids s) of
-                     Just id -> return id
-                     Nothing -> let id = "_x" ++ show (Map.size (env_ids s)) in
-                                let s' = Env { env_ids   = Map.insert exp id $ env_ids s
-                                             , env_in    = env_in s
-                                             , env_out   = env_out s
-                                             , env_sizes = Map.insert id n $ env_sizes s
-                                             }
-                                in
-                                put s' >> return id
-
-input :: Ident -> Integer -> Jazz Argument
-input id n = do s <- get
-                let s' = Env { env_ids   = env_ids s
-                             , env_in    = Set.insert id (env_in s)
-                             , env_out   = env_out s
-                             , env_sizes = Map.insert id n (env_sizes s)
-                             }
-                put s' >> return (ArgVar id)
-
-output :: Ident -> Integer -> Jazz Argument -> Jazz ()
-output id n x = do a <- x
-                   let exp = Earg a
-                   s <- get
-                   let s' = Env { env_ids   = Map.insert exp id $ env_ids s
-                                , env_in    = env_in s
-                                , env_out   = Set.insert id $ env_out s
-                                , env_sizes = Map.insert id n $ env_sizes s
-                                }
-                   put s'
+add_exp exp n =
+  get >>= \s ->
+  case Map.lookup exp (env_ids s) of
+    Just id -> return id
+    Nothing -> let id = "_x" ++ show (Map.size (env_ids s)) in
+               let s' = Env { env_ids   = Map.insert exp id $ env_ids s
+                            , env_in    = env_in s
+                            , env_out   = env_out s
+                            , env_sizes = Map.insert id n $ env_sizes s
+                            }
+               in
+               put s' >> return id
 
 arg_size :: Argument -> Jazz Integer
 arg_size (ArgCst v) = return (List.genericLength v)
@@ -57,93 +61,112 @@ arg_size (ArgVar id) = do s <- get
                           let n = (env_sizes s) ! id
                           return n
 
-arg :: Argument -> Jazz Argument
-arg = return
+input :: Ident -> Integer -> Jazz [Argument]
+input id n = do s <- get
+                let s' = Env { env_ids   = env_ids s
+                             , env_in    = Set.insert id (env_in s)
+                             , env_out   = env_out s
+                             , env_sizes = Map.insert id n (env_sizes s)
+                             }
+                put s' >> smash (n, ArgVar id)
 
-argvar :: Ident -> Jazz Argument
-argvar id = return (ArgVar id)
+output :: Bit a => Ident -> [a] -> Jazz ()
+output id l = do w <- mapM funk l
+                 (n, a) <- funnel w
+                 let exp = Earg a
+                 s <- get
+                 let s' = Env { env_ids   = Map.insert exp id $ env_ids s
+                              , env_in    = env_in s
+                              , env_out   = Set.insert id $ env_out s
+                              , env_sizes = Map.insert id n $ env_sizes s
+                              }
+                 put s'
 
-argcst :: Value -> Jazz Argument
-argcst v = return (ArgCst v)
+read_reg :: Ident -> Integer -> Jazz [Argument]
+read_reg id n = smash (n, ArgVar id)
 
-neg :: Jazz Argument -> Jazz Argument
-neg x = do a <- x
+write_reg :: Bit a => Ident -> [a] -> Jazz ()
+write_reg id x = do (n, ArgVar id') <- mapM funk x >>= funnel
+                    let exp = Ereg id'
+                    s <- get
+                    let s' = Env { env_ids   = Map.insert exp id (env_ids s)
+                                 , env_in    = env_in s
+                                 , env_out   = Set.insert id (env_out s)
+                                 , env_sizes = Map.insert id n (env_sizes s)
+                                 }
+                    put s'
+
+neg :: Bit a => a -> Jazz Argument
+neg x = do a <- funk x
            let exp = Enot a
            n <- arg_size a
            id <- add_exp exp n
            return (ArgVar id)
 
-reg :: Ident -> Integer -> Jazz Argument -> Jazz Argument
-reg id n x = do (ArgVar id') <- x
-                let exp = Ereg id'
-                s <- get
-                let s' = Env { env_ids   = Map.insert exp id $ env_ids s
-                             , env_in    = env_in s
-                             , env_out   = Set.insert id $ env_out s
-                             , env_sizes = Map.insert id n $ env_sizes s
-                             }
-                put s' >> return (ArgVar id')
-
-binop :: BinOp -> Jazz Argument -> Jazz Argument -> Jazz Argument
-binop op x y = do a <- x
-                  b <- y
+binop :: (Bit a, Bit b) => BinOp -> a -> b -> Jazz Argument
+binop op x y = do a <- funk x
+                  b <- funk y
                   let exp = Ebinop op a b
                   n <- arg_size a
                   id <- add_exp exp n
                   return (ArgVar id)
 
-mux :: Jazz Argument -> Jazz Argument -> Jazz Argument -> Jazz Argument
-mux x y z = do a <- x
-               b <- y
-               c <- z
-               let exp = Emux a b c
-               n <- arg_size b
-               id <- add_exp exp n
-               return (ArgVar id)
-
-rom :: Jazz Argument -> Jazz Argument
-rom x = do a <- x
-           let exp = Erom 32 8 a
-           let n = 8
-           id <- add_exp exp n
-           return (ArgVar id)
-
-ram :: Jazz Argument -> Jazz Argument
-    -> Jazz Argument -> Jazz Argument -> Jazz Argument
-ram ra we wa d = do a1 <- ra
-                    a2 <- we
-                    a3 <- wa
-                    a4 <- d
-                    let exp = Eram 32 8 a1 a2 a3 a4
-                    let n = 8
-                    id <- add_exp exp n
-                    return (ArgVar id)
-
-conc :: Jazz Argument -> Jazz Argument -> Jazz Argument
-conc x y = do a <- x
-              b <- y
-              let exp = Econcat a b
-              n1 <- arg_size a
-              n2 <- arg_size b
-              let n = n1 + n2
-              id <- add_exp exp n
-              return (ArgVar id)
-
-slice :: Integer -> Integer -> Jazz Argument -> Jazz Argument
-slice i j x = do a <- x
-                 let exp = Eslice i j a
-                 let n = j-i
+mux :: (Bit a, Bit b, Bit c) => a -> [b] -> [c] -> Jazz [Argument]
+mux a xs ys = do a <- funk a
+                 (n, x) <- mapM funk xs >>= funnel
+                 (_, y) <- mapM funk ys >>= funnel
+                 let exp = Emux a x y
                  id <- add_exp exp n
-                 return (ArgVar id)
+                 smash (n, (ArgVar id))
 
-select :: Integer -> Jazz Argument -> Jazz Argument
-select i x = do a <- x
+rom :: Bit a => [a] -> Jazz [Argument]
+rom xs = do (_, a) <- mapM funk xs >>= funnel
+            let exp = Erom addr_size word_size a
+            id <- add_exp exp word_size
+            smash (word_size, ArgVar id)
+
+ram :: (Bit a, Bit b, Bit c, Bit d) => [a] -> b -> [c] -> [d] -> Jazz [Argument]
+ram ra we wa d = do (_, a) <- mapM funk ra >>= funnel
+                    b <- funk we
+                    (_, c) <- mapM funk wa >>= funnel
+                    (_, d) <- mapM funk d  >>= funnel
+                    let exp = Eram addr_size word_size a b c d
+                    id <- add_exp exp word_size
+                    smash (word_size, ArgVar id)
+
+(\/) :: (Bit a, Bit b) => a -> b -> Jazz Argument
+x \/ y = binop Or x y
+
+(/\) :: (Bit a, Bit b) => a -> b -> Jazz Argument
+x /\ y = binop And x y
+
+(<>) :: (Bit a, Bit b) => a -> b -> Jazz Argument
+x <> y = binop Xor x y
+
+conc :: (Bit a, Bit b) => [a] -> [b] -> Jazz [Argument]
+conc x y = do w1 <- mapM funk x
+              w2 <- mapM funk y
+              return $ w1 ++ w2
+
+funnel :: [Argument] -> Jazz (Integer, Argument)
+funnel (x:xs) = do let aux (n,a) b = do id <- add_exp (Econcat b a) (n+1)
+                                        return (n+1, ArgVar id)
+                   foldM aux (1,x) xs
+
+smash :: (Integer, Argument) -> Jazz [Argument]
+smash (n, x) = mapM (\i -> select i x) [0..n-1]
+
+select :: Bit a => Integer -> a -> Jazz Argument
+select i x = do a <- funk x
                 let exp = Eselect i a
                 let n = 1
                 id <- add_exp exp n
                 return (ArgVar id)
 
-build :: Funk -> Netlist
+squeeze :: Bit a => [a] -> Jazz [Argument]
+squeeze xs = mapM funk xs >>= funnel >>= smash
+
+build :: Jazz () -> Netlist
 build x =
   let env_empty = Env { env_ids   = Map.empty
                       , env_in    = Set.empty
@@ -163,30 +186,4 @@ build x =
              , netlist_in  = input
              , netlist_out = output
              }
-
-(\/) :: Jazz Argument -> Jazz Argument -> Jazz Argument
-x \/ y = binop Or x y
-
-(/\) :: Jazz Argument -> Jazz Argument -> Jazz Argument
-x /\ y = binop And x y
-
-(<>) :: Jazz Argument -> Jazz Argument -> Jazz Argument
-x <> y = binop Xor x y
-
-funnel :: [Argument] -> Jazz Argument
-funnel l = let x:xs = List.map arg l in
-           List.foldl (flip conc) x xs
-
-smash :: Argument -> Integer -> Jazz [Argument]
-smash x n =
-  let aux acc _ 0 = return acc
-      aux acc x i = do y <- select (i-1) x
-                       aux (y:acc) x (i-1)
-  in
-  aux [] (arg x) n
-
-squeeze :: [Argument] -> Jazz [Argument]
-squeeze xs = do let n = List.genericLength xs
-                y <- funnel xs
-                smash y n
 
