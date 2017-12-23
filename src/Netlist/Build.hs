e4fd573764b7281647ce3cd58e92c7cb16298bad
pnmdln <paul-nicolas.madelaine@ens.fr>
2017-12-23 16:08:05 +0100

    whole lotta change

--- src/Netlist/Build.hs
+++ src/Netlist/Build.hs
@@ -25,34 +25,42 @@ data Env = Env { env_ids   :: Map.Map Expression Ident
 
 type Jazz = State Env
 
-class Bit a where
-  bit :: a -> Jazz Argument
-
-class Wire a where
-  bits :: a -> Jazz [Argument]
-  wire :: a -> Jazz Argument
+newtype Bit = Bit Argument
+newtype Wire = Wire (Integer, Argument)
 
 
-instance Bit a => Wire [a] where
-  bits l = mapM bit l
-  wire l = mapM bit l >>= funnel >>= (\(_, a) -> return a)
+class Bt a where
+  bit :: a -> Jazz Bit
 
-instance Wire a => Wire (Jazz a) where
-  bits x = x >>= bits
-  wire x = x >>= wire
-
-instance Bit Argument where
+instance Bt Bit where
   bit x = return x
 
-instance Bit a => Bit (Jazz a) where
+instance Bt a => Bt (Jazz a) where
   bit x = x >>= bit
 
-instance Bit Bool where
-  bit x = return $ ArgCst [x]
+instance Bt Bool where
+  bit x = return $ Bit (ArgCst [x])
 
-instance Bit Integer where
-  bit 0 = return $ ArgCst [False]
-  bit 1 = return $ ArgCst [True]
+instance Bt Integer where
+  bit 0 = return $ Bit (ArgCst [False])
+  bit 1 = return $ Bit (ArgCst [True])
+
+
+class Wr a where
+  bits :: a -> Jazz [Bit]
+  wire :: a -> Jazz Wire
+
+instance Wr Wire where
+  bits = smash
+  wire x = return x
+
+instance Bt a => Wr [a] where
+  bits l = mapM bit l
+  wire l = mapM bit l >>= funnel
+
+instance Wr a => Wr (Jazz a) where
+  bits x = x >>= bits
+  wire x = x >>= wire
 
 add_exp :: Expression -> Integer -> Jazz Ident
 add_exp exp n =
@@ -74,18 +82,18 @@ arg_size (ArgVar id) = do s <- get
                           let n = (env_sizes s) ! id
                           return n
 
-input :: Ident -> Integer -> Jazz [Argument]
+input :: Ident -> Integer -> Jazz [Bit]
 input id n = do s <- get
                 let s' = Env { env_ids   = env_ids s
                              , env_in    = Set.insert id (env_in s)
                              , env_out   = env_out s
                              , env_sizes = Map.insert id n (env_sizes s)
                              }
-                put s' >> smash (n, ArgVar id)
+                put s' >> smash (Wire (n, ArgVar id))
 
-output :: Wire a => Ident -> a -> Jazz ()
+output :: Wr a => Ident -> a -> Jazz ()
 output id l = do w <- bits l
-                 (n, a) <- funnel w
+                 Wire (n, a) <- funnel w
                  let exp = Earg a
                  s <- get
                  let s' = Env { env_ids   = Map.insert exp id $ env_ids s
@@ -104,15 +112,15 @@ new_reg id n = do s <- get
                                }
                   put s'
 
-reg_out :: Ident -> Jazz [Argument]
+reg_out :: Ident -> Jazz [Bit]
 reg_out id = do s <- get
                 let exp = Ereg id
                 let n = (env_sizes s) ! id
                 id' <- add_exp exp n
-                smash (n, ArgVar id')
+                smash $ Wire (n, ArgVar id')
 
-reg_in :: Wire a => Ident -> a -> Jazz ()
-reg_in id x = do (n, ArgVar id') <- bits x >>= funnel
+reg_in :: Wr a => Ident -> a -> Jazz ()
+reg_in id x = do Wire (n, ArgVar id') <- bits x >>= funnel
                  let exp = Earg (ArgVar id')
                  s <- get
                  let s' = Env { env_ids   = Map.insert exp id (env_ids s)
@@ -122,76 +130,80 @@ reg_in id x = do (n, ArgVar id') <- bits x >>= funnel
                               }
                  put s'
 
-neg :: Bit a => a -> Jazz Argument
-neg x = do a <- bit x
+neg :: Bt a => a -> Jazz Bit
+neg x = do Bit a <- bit x
            let exp = Enot a
            n <- arg_size a
            id <- add_exp exp n
-           return (ArgVar id)
+           return $ Bit (ArgVar id)
 
-binop :: (Bit a, Bit b) => BinOp -> a -> b -> Jazz Argument
-binop op x y = do a <- bit x
-                  b <- bit y
+binop :: (Bt a, Bt b) => BinOp -> a -> b -> Jazz Bit
+binop op x y = do Bit a <- bit x
+                  Bit b <- bit y
                   let exp = Ebinop op a b
                   n <- arg_size a
                   id <- add_exp exp n
-                  return (ArgVar id)
+                  return $ Bit (ArgVar id)
 
-mux :: (Bit a, Wire b, Wire c) => a -> b -> c -> Jazz [Argument]
-mux a xs ys = do a <- bit a
-                 (ArgVar id') <- wire xs
-                 y <- wire ys
+mux :: (Bt a, Wr b, Wr c) => a -> b -> c -> Jazz [Bit]
+mux a xs ys = do Bit a <- bit a
+                 Wire (_, ArgVar id') <- wire xs
+                 Wire (_, y) <- wire ys
                  s <- get
                  let n = (env_sizes s) ! id'
                  let exp = Emux a (ArgVar id') y
                  id <- add_exp exp n
-                 smash (n, (ArgVar id))
+                 smash $ Wire (n, ArgVar id)
 
-rom :: Wire a => a -> Jazz [Argument]
-rom xs = do (_, a) <- bits xs >>= funnel
+rom :: Wr a => a -> Jazz [Bit]
+rom xs = do Wire (_, a) <- bits xs >>= funnel
             let exp = Erom a
             id <- add_exp exp word_size
-            smash (word_size, ArgVar id)
+            smash $ Wire (word_size, ArgVar id)
 
-ram :: (Wire a, Bit b, Wire c, Wire d) => a -> b -> c -> d -> Jazz [Argument]
-ram ra we wa d = do (_, a) <- bits ra >>= funnel
-                    b <- bit we
-                    (_, c) <- bits wa >>= funnel
-                    (_, d) <- bits d  >>= funnel
-                    let exp = Eram a b c d
-                    id <- add_exp exp word_size
-                    smash (word_size, ArgVar id)
+ram :: (Wr a, Bt b, Wr c, Wr d) => a -> b -> c -> d -> Jazz [Bit]
+ram ra we wa dt = do Wire (_, a) <- bits ra >>= funnel
+                     Bit b <- bit we
+                     Wire (_, c) <- bits wa >>= funnel
+                     Wire (_, d) <- bits dt >>= funnel
+                     let exp = Eram a b c d
+                     id <- add_exp exp word_size
+                     smash $ Wire (word_size, ArgVar id)
 
-(\/) :: (Bit a, Bit b) => a -> b -> Jazz Argument
+(\/) :: (Bt a, Bt b) => a -> b -> Jazz Bit
 x \/ y = binop Or x y
 
-(/\) :: (Bit a, Bit b) => a -> b -> Jazz Argument
+(/\) :: (Bt a, Bt b) => a -> b -> Jazz Bit
 x /\ y = binop And x y
 
-(<>) :: (Bit a, Bit b) => a -> b -> Jazz Argument
+(<>) :: (Bt a, Bt b) => a -> b -> Jazz Bit
 x <> y = binop Xor x y
 
-conc :: (Wire a, Wire b) => a -> b -> Jazz [Argument]
-conc x y = do w1 <- bits x
-              w2 <- bits y
-              return $ w1 ++ w2
+conc :: (Wr a, Wr b) => a -> b -> Jazz Wire
+conc x y = do Wire (n, a) <- wire x
+              Wire (m, b) <- wire y
+              let exp = Econcat b a
+              id <- add_exp exp (n+m)
+              return $ Wire (n+m, ArgVar id)
 
-funnel :: [Argument] -> Jazz (Integer, Argument)
-funnel (x:xs) = do let aux (n,a) b = do id <- add_exp (Econcat b a) (n+1)
-                                        return (n+1, ArgVar id)
-                   foldM aux (1,x) xs
+funnel :: [Bit] -> Jazz Wire
+funnel ((Bit x):xs) =
+  let aux (Wire (n,a)) (Bit b) = do id <- add_exp (Econcat b a) (n+1)
+                                    return $ Wire ((n+1), (ArgVar id))
+  in
+  foldM aux (Wire (1, x)) xs
 
-smash :: (Integer, Argument) -> Jazz [Argument]
-smash (n, x) = mapM (\i -> select i x) [0..n-1]
+smash :: Wire -> Jazz [Bit]
+smash (Wire (n, x)) = mapM (\i -> select i (Bit x)) [0..n-1]
 
-select :: Bit a => Integer -> a -> Jazz Argument
-select i x = do a <- bit x
+select :: Bt a => Integer -> a -> Jazz Bit
+select i x = do Bit a <- bit x
                 let exp = Eselect i a
                 let n = 1
                 id <- add_exp exp n
-                return (ArgVar id)
+                return $ Bit (ArgVar id)
 
-squeeze :: Wire a => a -> Jazz [Argument]
+squeeze :: Wr a => a -> Jazz [Bit]
 squeeze xs = bits xs >>= funnel >>= smash
 
 build :: Jazz () -> Netlist
