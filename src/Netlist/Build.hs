ff494014ba05eaf33d09c4d54b84aad564cded50
pnmdln <paul-nicolas.madelaine@ens.fr>
2017-12-23 15:00:14 +0100

    reimplemented the registers ; modified the type classes

--- src/Netlist/Build.hs
+++ src/Netlist/Build.hs
@@ -26,32 +26,33 @@ data Env = Env { env_ids   :: Map.Map Expression Ident
 type Jazz = State Env
 
 class Bit a where
-  funk :: a -> Jazz Argument
+  bit :: a -> Jazz Argument
+
 class Wire a where
-  prog :: a -> Jazz [Argument]
+  bits :: a -> Jazz [Argument]
+  wire :: a -> Jazz Argument
+
 
 instance Bit a => Wire [a] where
-  prog l = mapM funk l
+  bits l = mapM bit l
+  wire l = mapM bit l >>= funnel >>= (\(_, a) -> return a)
 
 instance Wire a => Wire (Jazz a) where
-  prog x = do
-    l <- x
-    prog l
+  bits x = x >>= bits
+  wire x = x >>= wire
 
 instance Bit Argument where
-  funk x = return x
+  bit x = return x
 
 instance Bit a => Bit (Jazz a) where
-  funk x = do
-    y <- x
-    funk y
+  bit x = x >>= bit
 
 instance Bit Bool where
-  funk x = return $ ArgCst [x]
+  bit x = return $ ArgCst [x]
 
 instance Bit Integer where
-  funk 0 = return $ ArgCst [False]
-  funk 1 = return $ ArgCst [True]
+  bit 0 = return $ ArgCst [False]
+  bit 1 = return $ ArgCst [True]
 
 add_exp :: Expression -> Integer -> Jazz Ident
 add_exp exp n =
@@ -83,7 +84,7 @@ input id n = do s <- get
                 put s' >> smash (n, ArgVar id)
 
 output :: Wire a => Ident -> a -> Jazz ()
-output id l = do w <- prog l
+output id l = do w <- bits l
                  (n, a) <- funnel w
                  let exp = Earg a
                  s <- get
@@ -94,54 +95,69 @@ output id l = do w <- prog l
                               }
                  put s'
 
-read_reg :: Ident -> Integer -> Jazz [Argument]
-read_reg id n = smash (n, ArgVar id)
+new_reg :: Ident -> Integer -> Jazz ()
+new_reg id n = do s <- get
+                  let s' = Env { env_ids   = env_ids s
+                               , env_in    = env_in s
+                               , env_out   = Set.insert id $ env_out s
+                               , env_sizes = Map.insert id n $ env_sizes s
+                               }
+                  put s'
 
-write_reg :: Wire a => Ident -> a -> Jazz ()
-write_reg id x = do (n, ArgVar id') <- prog x >>= funnel
-                    let exp = Ereg id'
-                    s <- get
-                    let s' = Env { env_ids   = Map.insert exp id (env_ids s)
-                                 , env_in    = env_in s
-                                 , env_out   = Set.insert id (env_out s)
-                                 , env_sizes = Map.insert id n (env_sizes s)
-                                 }
-                    put s'
+reg_out :: Ident -> Jazz [Argument]
+reg_out id = do s <- get
+                let exp = Ereg id
+                let n = (env_sizes s) ! id
+                id' <- add_exp exp n
+                smash (n, ArgVar id')
+
+reg_in :: Wire a => Ident -> a -> Jazz ()
+reg_in id x = do (n, ArgVar id') <- bits x >>= funnel
+                 let exp = Earg (ArgVar id')
+                 s <- get
+                 let s' = Env { env_ids   = Map.insert exp id (env_ids s)
+                              , env_in    = env_in s
+                              , env_out   = env_out s
+                              , env_sizes = env_sizes s
+                              }
+                 put s'
 
 neg :: Bit a => a -> Jazz Argument
-neg x = do a <- funk x
+neg x = do a <- bit x
            let exp = Enot a
            n <- arg_size a
            id <- add_exp exp n
            return (ArgVar id)
 
 binop :: (Bit a, Bit b) => BinOp -> a -> b -> Jazz Argument
-binop op x y = do a <- funk x
-                  b <- funk y
+binop op x y = do a <- bit x
+                  b <- bit y
                   let exp = Ebinop op a b
                   n <- arg_size a
                   id <- add_exp exp n
                   return (ArgVar id)
 
 mux :: (Bit a, Wire b, Wire c) => a -> b -> c -> Jazz [Argument]
-mux a xs ys = do a <- funk a
-                 (n, x) <- prog xs >>= funnel
-                 (_, y) <- prog ys >>= funnel
-                 let exp = Emux a x y
+mux a xs ys = do a <- bit a
+                 (ArgVar id') <- wire xs
+                 y <- wire ys
+                 s <- get
+                 let n = (env_sizes s) ! id'
+                 let exp = Emux a (ArgVar id') y
                  id <- add_exp exp n
                  smash (n, (ArgVar id))
 
 rom :: Wire a => a -> Jazz [Argument]
-rom xs = do (_, a) <- prog xs >>= funnel
+rom xs = do (_, a) <- bits xs >>= funnel
             let exp = Erom a
             id <- add_exp exp word_size
             smash (word_size, ArgVar id)
 
 ram :: (Wire a, Bit b, Wire c, Wire d) => a -> b -> c -> d -> Jazz [Argument]
-ram ra we wa d = do (_, a) <- prog ra >>= funnel
-                    b <- funk we
-                    (_, c) <- prog wa >>= funnel
-                    (_, d) <- prog d  >>= funnel
+ram ra we wa d = do (_, a) <- bits ra >>= funnel
+                    b <- bit we
+                    (_, c) <- bits wa >>= funnel
+                    (_, d) <- bits d  >>= funnel
                     let exp = Eram a b c d
                     id <- add_exp exp word_size
                     smash (word_size, ArgVar id)
@@ -156,8 +172,8 @@ x /\ y = binop And x y
 x <> y = binop Xor x y
 
 conc :: (Wire a, Wire b) => a -> b -> Jazz [Argument]
-conc x y = do w1 <- prog x
-              w2 <- prog y
+conc x y = do w1 <- bits x
+              w2 <- bits y
               return $ w1 ++ w2
 
 funnel :: [Argument] -> Jazz (Integer, Argument)
@@ -169,14 +185,14 @@ smash :: (Integer, Argument) -> Jazz [Argument]
 smash (n, x) = mapM (\i -> select i x) [0..n-1]
 
 select :: Bit a => Integer -> a -> Jazz Argument
-select i x = do a <- funk x
+select i x = do a <- bit x
                 let exp = Eselect i a
                 let n = 1
                 id <- add_exp exp n
                 return (ArgVar id)
 
 squeeze :: Wire a => a -> Jazz [Argument]
-squeeze xs = prog xs >>= funnel >>= smash
+squeeze xs = bits xs >>= funnel >>= smash
 
 build :: Jazz () -> Netlist
 build x =
