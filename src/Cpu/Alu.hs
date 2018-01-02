f0b58141f660f785c7f03772b20b4f0db1485bb0
pnmdln <paul-nicolas.madelaine@ens.fr>
2018-01-02 11:58:20 +0100

    added sll, srl, sra and lui

--- src/Cpu/Alu.hs
+++ src/Cpu/Alu.hs
@@ -22,17 +22,62 @@ data Alu_flag = Alu_flag { carry_out :: Bit
 -- Or  -> alu_force_or, alu_invert_x
 -- Xor -> alu_
 
-alu :: (Wr a, Wr b) => Instr -> a -> b -> Jazz (Alu_flag, [Bit])
+alu :: (Wr a, Wr b) => Instr -> a -> b -> Jazz (Alu_flag, Wire)
 alu instr x y = do
   xs <- bits x
   ys <- bits y
   ctrl <- nalu_control instr
-  (c_out, z) <- nalu ctrl xs ys
-  zero <- isZero z
+  (c_out, nalu) <- nalu ctrl xs ys
+  zero <- wire (32 :: Integer, 0 :: Integer)
+  let imm = instr_imm instr
+  let ctrl_mux = Opcode_mux { op_j       = wire zero
+                            , op_jal     = wire nalu
+                            , op_beq     = wire nalu
+                            , op_bne     = wire nalu
+                            , op_addi    = wire nalu
+                            , op_addiu   = wire nalu
+                            , op_slti    = wire zero
+                            , op_sltiu   = wire zero
+                            , op_andi    = wire nalu
+                            , op_ori     = wire nalu
+                            , op_lui     = conc (List.replicate 16 False) imm
+                            , op_lw      = wire zero
+                            , op_lbu     = wire zero
+                            , op_lhu     = wire zero
+                            , op_sb      = wire zero
+                            , op_sh      = wire zero
+                            , op_sw      = wire zero
+                            , op_ll      = wire zero
+                            , op_sc      = wire zero
+
+                            , op_sll     = shift True False (instr_shamt instr) x
+                            , op_srl     = shift False False (instr_shamt instr) x
+                            , op_sra     = shift False True (instr_shamt instr) x
+                            , op_jr      = wire zero
+                            , op_mfhi    = wire zero
+                            , op_mflo    = wire zero
+                            , op_mult    = wire zero
+                            , op_multu   = wire zero
+                            , op_div     = wire zero
+                            , op_divu    = wire zero
+                            , op_add     = wire nalu
+                            , op_addu    = wire nalu
+                            , op_sub     = wire nalu
+                            , op_subu    = wire nalu
+                            , op_and     = wire nalu
+                            , op_or      = wire nalu
+                            , op_nor     = wire nalu
+                            , op_slt     = wire zero
+                            , op_sltu    = wire zero
+
+                            , op_nop     = wire zero
+                            }
+  res <- opcode_mux instr ctrl_mux
+  res_zero <- isZero res
   return ( Alu_flag { carry_out = c_out
-                    , is_zero = zero
+                    , is_zero = res_zero
                     }
-         , z
+         , res
          )
 
 --doShift :: Instr -> Jazz Bit
@@ -47,9 +92,9 @@ nalu_inputs instr = do
   sign_ext_imm <- conc imm (List.replicate 16 (select 15 imm))
   zero_ext_imm <- conc imm (List.replicate 16 False)
   branch_addr <- conc [False, False] $ conc imm (List.replicate 14 (select 15 imm))
-  let zero = wire (32 :: Integer, 0 :: Integer)
+  zero <- wire (32 :: Integer, 0 :: Integer)
   let ctrl_mux = Opcode_mux { op_j       = conc zero zero
-                            , op_jal     = conc pc (32 :: Integer, 8 :: Integer)
+                            , op_jal     = conc pc (32 :: Integer, 4 :: Integer)
                             , op_beq     = conc pc branch_addr
                             , op_bne     = conc pc branch_addr
                             , op_addi    = conc rs zero_ext_imm
