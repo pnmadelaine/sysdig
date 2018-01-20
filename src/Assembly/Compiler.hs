00d76b1546624371be4fd47cb88302e8aa6f40f1
pnmdln <paul-nicolas.madelaine@ens.fr>
2018-01-20 15:57:22 +0100

    working assembly compiler (?)

--- src/Assembly/Compiler.hs
+++ src/Assembly/Compiler.hs
@@ -4,18 +4,25 @@ import qualified Data.List as List
 import Assembly.Ast
 
 print_tab :: [Bool] -> String
-print_tab [] = ""
-print_tab l = (aux (List.head l)) ++ (print_tab (List.tail l))
-    where aux True = "1"
-          aux False = "0"
+print_tab l = List.reverse . List.map aux $ l
+    where aux True = '1'
+          aux False = '0'
 
 
 print_instr :: Instr -> String
-print_instr (Rexpr o s t d shamt funct) = (print_tab funct) ++ (print_tab shamt) ++ (print_tab d) ++ (print_tab t) ++ (print_tab s) ++ (print_tab o)
-print_instr (Iexpr o s t imm) = (print_tab imm) ++ (print_tab t) ++ (print_tab s) ++ (print_tab o)
-print_instr (Jump o a) = (print_tab a) ++ (print_tab o)
-print_instr _ = error "wrong instruction"
+print_instr i =
+  aux2 0 . aux1 $ i
+    where aux1 (Rexpr opcode rs rt rd shamt funct) =
+            List.concat . List.map print_tab $ [opcode, rs, rt, rd, shamt, funct]
+          aux1 (Iexpr opcode rs rt imm) =
+            List.concat . List.map print_tab $ [opcode, rs, rt, imm]
+          aux1 (Jump opcode address) =
+            List.concat . List.map print_tab $ [opcode, address]
+          aux1 _ = error "wrong instruction"
 
+          aux2 _ [] = []
+          aux2 8 l  = ' ':(aux2 0 l)
+          aux2 i (x:xs) = x:(aux2 (i+1) xs)
 
 print_prog :: Prog -> String
-print_prog p = List.unlines (List.map print_instr p)
+print_prog p = List.unlines . List.map print_instr . List.reverse $ p
