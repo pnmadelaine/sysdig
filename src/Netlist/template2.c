88bd0d5ab9e9148509be165fe55397a2e1b0d176
antonin-r <antonin.reitz@ens.fr>
2018-01-26 17:40:37 +0100

    final version, templates 1-3 C, multithread

---
+++ src/Netlist/template2.c
@@ -0,0 +1,29 @@
+
+    int* _ram = malloc(sizeof(int) * _ram_size);
+
+    pthread_t myThreadSimulator;
+    pthread_t myThreadPrinter;
+
+    pthread_create(&myThreadSimulator, NULL, threadSimulator, _ram);
+    pthread_create(&myThreadPrinter, NULL, threadPrinter, _ram);
+
+    pthread_join(myThreadSimulator, NULL);
+    pthread_join(myThreadPrinter, NULL);
+
+
+    return 0;
+}
+
+void* threadPrinter (int* _ram)
+{
+    while(1) {
+        f(_ram[0], _ram[4], _ram[8], _ram[12], _ram[20], _ram[24]);
+        usleep (20000);
+    }
+    pthread_exit(NULL);
+}
+
+
+void* threadSimulator (int* _ram)
+{
+	long int _n = 100000000;
---
+++ src/Netlist/template3.c
@@ -0,0 +1,2 @@
+    pthread_exit(NULL);
+}
