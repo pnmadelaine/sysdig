ef541989f0d2931f34c2adbe610e1b3cc07b8e6c
antonin-r <antonin.reitz@ens.fr>
2018-03-30 23:14:07 +0200

    updates

--- Makefile
+++ Makefile
@@ -9,16 +9,20 @@ all:
 	./optimizer cpu.net
 	./assembly clock.s
 	./assembly fast_clock.s
+	./assembly incr.s
 	./compiler cpu_opt.net clock
 	mv cpu_opt.c main1.c
-	gcc main1.c -o clock_exec -Ofast -pthread -lpthread
+	gcc main1.c -o clock_exec -w -Ofast -pthread -lpthread
 	./compiler cpu_opt.net fast_clock
 	mv cpu_opt.c main2.c
-	gcc main2.c -o fast_clock_exec -Ofast -pthread -lpthread
+	gcc main2.c -o fast_clock_exec -w -Ofast -pthread -lpthread
+	./compiler cpu_opt.net incr
+	mv cpu_opt.c main3.c
+	gcc main3.c -o incr_exec -w -Ofast -pthread -lpthread
 
 clean:
 	cabal clean
 	rm simulator generator optimizer compiler assembly
 	rm cpu.net cpu_opt.net
-	rm main1.c main2.c
-	rm clock_exec fast_clock_exec
+	rm main1.c main2.c main3.c
+	rm clock_exec fast_clock_exec incr
--- clock.s
+++ clock.s
@@ -1,142 +1,176 @@
 main:
+  j gettime
+  
+gettime:
+  lw $at,$zero,28
+  li $t0,0
+  li $t1,0
+  li $t2,0
+  li $t3,1
+  li $t4,3
+  li $t5,1
+  li $t6,1970
+  
+  lui $ra, 1926
+  addiu $ra,$ra,8064
+  divu $at,$ra
+  mflo $a1
+  sll $ra,$a1,2
+  addu $t6,$t6,$ra
+  
+  mfhi $fp
+  lui $ra, 481
+  addiu $ra,$ra,13184
+  divu $fp,$ra
+  mflo $a2
+  addu $t6,$t6,$a2 
+    
+  mfhi $ra
+  subu $fp,$at,$ra
+  
+  li $ra,5
+  multu $ra,$a1
+  mflo $ra
+  addu $t4,$t4,$ra  
+  addu $t4,$t4,$a2
+  
+  li $ra,7
+  divu $t4,$ra
+  mfhi $t4
+  
   j init
- 
-ganvier:
-  j nd_31
-  
-fevrier:
-  j nd_fevrier
-  
-mars:
-  j nd_31
-  
-avril:
-  j nd_30
-  
-mai:
-  j nd_31
-  
-guin:
-  j nd_30
-  
-guillet:
-  j nd_31
-  
-aout:
-  j nd_31
-  
-septembre:
-  j nd_30
-  
-octobre:
-  j nd_31
-  
-novembre:
-  j nd_30
-  
-decembre:
-  j nd_31
   
   
 init:
+  sw $t3,$zero,12
+  sw $t5,$zero,20
+  sw $t6,$zero,24
+
   li $a0,60
   li $a1,24
   li $a2, 7
   
-  li $v0, 28
-  li $v1, 29
-  li $k0, 30
-  li $k1, 31
+  li $v0, 29
+  li $v1, 30
+  li $k0, 31
+  li $k1, 32
   
-  li $a3,12
+  li $a3,13
   
-  li $gp,4
   li $sp,100
-  li $fp,400
   
   j second
 
 second:
+  lw $at,$zero,28
+  beq $at,$fp,second
+
+  addiu $fp,$fp,1
   addiu $t0,$t0,1
-  divu $t0,$a0
-  mfhi $t0
-  beq $t0,$zero,minute
+  beq $t0,$a0,minute
+  sw $t0,$zero,0
   j second
   
   
 minute:
+  li $t0,0
+  sw $t0,$zero,0
   addiu $t1,$t1,1
-  divu $t1,$a0
-  mfhi $t1
-  beq $t1,$zero,hour
+  beq $t1,$a0,hour
+  sw $t1,$zero,4
   j second
   
 hour:
+  li $t1,0
+  sw $t1,$zero,4
   addiu $t2,$t2,1
-  divu $t2,$a1
-  mfhi $t2
-  beq $t2,$zero,day
+  beq $t2,$a1,day
+  sw $t2,$zero,8
   j second
   
-day:  
-  addiu $t4,$t4,1
-  divu $t4,$a2
-  mfhi $t4
-  
+day:
+  li $t2,0  
+  sw $t2,$zero,8
+  addiu $t4,$t4,1 
+
   addiu $t3,$t3,1
-  addi $ra,$t5,1
-  jr $ra
- 
-nd_28:
-  divu $t3,$v0
-  mfhi $t3
-  beq $t3,$zero,month
-  j second 
-  
-nd_29:
-  divu $t3,$v1
-  mfhi $t3
-  beq $t3,$zero,month
-  j second
+  sw $t3,$zero,12
+
+  beq $t4,$a2,week  
+  sw $t4,$zero,16
+  j machin
+
+week:
+  li $t4,0  
+  sw $t4,$zero,16
+  j machin
+
+machin:
+  li $ra,1
+  beq $ra,$t5,nd_31
+  li $ra,2
+  beq $ra,$t5,nd_fevrier
+  li $ra,3
+  beq $ra,$t5,nd_31
+  li $ra,4
+  beq $ra,$t5,nd_30
+  li $ra,5
+  beq $ra,$t5,nd_31
+  li $ra,6
+  beq $ra,$t5,nd_30
+  li $ra,7
+  beq $ra,$t5,nd_31
+  li $ra,8
+  beq $ra,$t5,nd_31
+  li $ra,9
+  beq $ra,$t5,nd_30
+  li $ra,10
+  beq $ra,$t5,nd_31
+  li $ra,11
+  beq $ra,$t5,nd_30
+  li $ra,12
+  beq $ra,$t5,nd_31
   
 nd_30:
-  divu $t3,$k0
-  mfhi $t3
-  beq $t3,$zero,month
+  beq $t3,$k0,month
   j second
   
 nd_31:
-  divu $t3,$k1
-  mfhi $t3
-  beq $t3,$zero,month
+  beq $t3,$k1,month
   j second
   
 nd_fevrier:
-  divu $t6,$gp
-  mfhi $ra
+
+  beq $t3,$v1,month
+  bne $t3,$v0,second
+
+  andi $ra,$t6,3
   beq $ra,$zero,bissextile
-  j nd_28
+  j month
   
 bissextile:
   divu $t6,$sp
   mfhi $ra
   beq $ra,$zero,cent_bissextile
-  j nd_29
+  j second
   
 cent_bissextile:
-  divu $t6,$fp
-  mfhi $ra
-  beq $ra,$zero,nd_29
-  j nd_28
+  mflo $ra
+  andi $ra,$ra,3
+  beq $ra,$zero,second
+  j month
   
 month:
+  li $t3,1
+  sw $t3,$zero,12
   addiu $t5,$t5,1
-  divu $t5,$a3
-  mfhi $t5
-  beq $t5,$zero,year
+  beq $t5,$a3,year
+  sw $t5,$zero,20
   j second
   
 year:
-  addiu $t6,$t6,1
+  li $t5,1
+  sw $t5,$zero,20
+  addiu $t6,$t6,1  
+  sw $t6,$zero,24
   j second
-  
